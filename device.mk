26c1da910b5a5aa02e1cd78d79968e958b5ef800
Shas45558 <you@example.com>
2023-08-19 16:51:34 +0600

    Try without vendor

--- device.mk
+++ device.mk
@@ -65,4 +65,4 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 # Inherit the proprietary files
-$(call inherit-product, vendor/xiaomi/lancelot/lancelot-vendor.mk)
+# $(call inherit-product, vendor/xiaomi/lancelot/lancelot-vendor.mk)
